768e464adeeef5d2cbaf66fdf319f966d70b22a5
JunYoung Choi <h0ch1@naver.com>
2024-10-01 14:55:46 +0900

    first

---
+++ src/main/java/com/example/crewup/CrewupApplication.java
@@ -0,0 +1,13 @@
+package com.example.crewup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crewup/CrewupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crewup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
